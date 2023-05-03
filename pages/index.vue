8dad6a4369ac870d9fe6f5fceed87f47c3a61e6b
fumiyadoi <38125676+fumiyadoi@users.noreply.github.com>
2023-05-03 23:18:15 +0900

    feature: 入力部分作成

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>ChatGPTアプリ</h1>
+  <div>
+    <input type="text" v-model="keyword" />
+  </div>
+  <div>
+    <button type="button" @click="handleClick">回答を作成する</button>
+  </div>
+</template>
+
+<script setup>
+const keyword = ref("");
+
+const handleClick = () => {
+  // ここに生成する処理をかく
+};
+</script>
