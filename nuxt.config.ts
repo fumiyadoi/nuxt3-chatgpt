9dbf2851243b7920131d5e12d304bb6ab7b05965
fumiyadoi <38125676+fumiyadoi@users.noreply.github.com>
2023-05-05 15:51:57 +0900

    chore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  pages: true,
-  routeRules: {
-    "/**": { static: true },
-    "/posts/1": { static: true },
-    "/posts/**": { static: true },
+  nitro: {
+    preset: "vercel-edge",
   },
 });
